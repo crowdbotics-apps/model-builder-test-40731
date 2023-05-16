e249e29eb5ceaebac168f3ee2de4ad685ce45c93
Crowdbotics <team@crowdbotics.com>
2023-05-16 15:36:29 +0000

    Initial commit.

---
+++ backend/model_builder_test_40731/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for model_builder_test_40731 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'model_builder_test_40731.settings')
+
+application = get_wsgi_application()
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "model-builder-test-40731",
+  url: "https://model-builder-test-40731.botics.co",
+  api: "https://model-builder-test-40731.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
